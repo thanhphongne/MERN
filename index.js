eb365b00b554760677ece1099b5222fcff0dc9ed
thanhphong <btphongc3tl@gmail.com>
2021-12-18 08:41:56 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors')
+
+const userRouter = require('./routes/user');
+const authRouter = require('./routes/auth');
+const productRouter = require('./routes/product');
+const cartRouter = require('./routes/cart');
+const orderRouter = require('./routes/order');
+const stripeRoute = require("./routes/stripe");
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connection Successfully"))
+    .catch((err) => console.log("DB Connection Failure "+err));
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/carts', cartRouter);
+app.use('/api/orders', orderRouter);
+app.use("/api/checkout", stripeRoute);
+
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log("Backend server is running")
+})
